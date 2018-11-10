b82abfbb18914749a9060eeb8ae0603a8da7ac5b
Krzysiek Przygudzki <kpr@touk.pl>
2018-11-10 23:40:38 +0100

    Move rendering of entire application to one method

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 import { Char, allChars } from './chars.js'
 
-const getRandom = (max) => Math.floor(Math.random() * Math.floor(max));
-const randomElement = (array) => array[getRandom(array.length)];
-const flattenArrayOfArrays = (array) => array.reduce((acc, el) => [...acc, ...el])
-
 const state = {
 	currentChar: undefined,
 	charsInPlay: allChars,
 	result: undefined
 };
-
-const charsInPlay = [...allChars];
-var currentChar;
 
 const skip = () => {
 	state.result = undefined;
@@ -21,44 +14,50 @@ const guess = (entry) => {
 	const isAnswerCorrect = validateGuess(entry);
 	state.result = isAnswerCorrect;
 	if (isAnswerCorrect) { generateNext(); }
-	else { renderButtons(); }
+	else { render(state); }
 }
 const validateGuess = (guess) => guess === state.currentChar.latin;
 const guessResult = (bool) => bool ? "Good job!" : "Wrong...";
 
 const generateNext = () => {
 	state.currentChar = getNextChar(state.charsInPlay, state.currentChar);
-	setContent(elementHandle, state.currentChar.hiragana);
-	renderButtons();
+	render(state);
 }
 const getNextChar = (charsInPlay, currentChar) => {
 	const element = randomElement(flattenArrayOfArrays(charsInPlay).filter(char => char !== currentChar));
 	return element ? element : currentChar;
 };
 
-window.skip = skip;
-window.guess = guess;
+const containerHandle = document.getElementById("container");
 
-const elementHandle = document.getElementById("character");
-const resultHandle = document.getElementById("result");
-const guessButtonsHandle = document.getElementById("guessButtons");
-const buttonsHandle = document.getElementById("buttons");
-
-const renderButtons = () => {
-	const output = resultTile(state.result) + skipButton() + guessButtons(state.charsInPlay);
-	setContent(buttonsHandle, output);
-};
-const renderInitialButtons = () => {
-	const output = emptyResultTile() + skipButton();
-	setContent(buttonsHandle, output);
+const render = (state) => {
+	const output = containerRender(state);
+	setContent(containerHandle, output);
 }
+
+const containerRender = (state) => {
+	return headerRender() + charDisplayRender(state.currentChar) + buttonsRender() + footerRender()
+};
+
+const headerRender = () => '<div class="header"><h1>Hiragana Practice</h1></div>';
+const charDisplayRender = (char) => {
+	const hiraganaChar = char ? char.hiragana : '&nbsp;';
+	return '<div id="charDisplay" class="display">' + '<div id="character">' + hiraganaChar + '</div>' + '</div>';
+};
+const buttonsRender = () => {
+	if (state.currentChar === undefined) {
+		return '<div id="buttons" class="buttons">' + emptyResultTile() + skipButton() + '</div>';
+	}
+	return '<div id="buttons" class="buttons">' + resultTile(state.result) + skipButton() + guessButtons(state.charsInPlay) + '</div>'
+};
+const footerRender = () => '<div class="footer"></div>';
 
 const skipButton = () => '<button id="generate" onclick="skip()">Generate</button>';
 const guessButtons = (charGroups) => '<div id="guessButtons" class="guessButtons">' + charGroups.map(charGroup => guessButtonGroup(charGroup)).join('') + '</div>';
 const guessButtonGroup = (chars) => '<div class="guessButtonsGroup">' + chars.map(char => guessButton(char)).join('') + '</div>';
 const guessButton = (char) => '<button type="button" onClick="guess(\'' + char.latin + '\')" class="guessButton">' + char.latin + '</button>';
 
-const resultTile = (bool) => bool === undefined ? emptyResultTile() : bool ? correctResultTile() : wrongResultTile();
+const resultTile = (result) => result === undefined ? emptyResultTile() : result ? correctResultTile() : wrongResultTile();
 const correctResultTile = () => wrapWithDiv('Good job!');
 const wrongResultTile = () => wrapWithDiv('Wrong...');
 const emptyResultTile = () => wrapWithDiv('&nbsp');
@@ -69,8 +68,11 @@ const setContent = (handle, value) => {
 	handle.innerHTML = value;
 };
 
-const clearContent = (handle) => {
-	handle.innerHTML = "&nbsp;";
-};
+const getRandom = (max) => Math.floor(Math.random() * Math.floor(max));
+const randomElement = (array) => array[getRandom(array.length)];
+const flattenArrayOfArrays = (array) => array.reduce((acc, el) => [...acc, ...el])
 
-renderInitialButtons();
+window.skip = skip;
+window.guess = guess;
+
+render(state);
